6867a8876ea05ace83d8d50032cec611f6ba7405
AdamB <abreece@gmail.com>
2014-04-22 15:23:12 -0700

    Comment cleanup.

--- run_analysis.R
+++ run_analysis.R
@@ -42,8 +42,9 @@ setnames(mergedData, featureNames)
 ## Step 2: Extract only the measurements on the mean and 
 ## 			 standard deviation for each measurement.
 
-# Find column names missing -mean() or -std(), and remove them
-# Note: this includes removing meanfreq() columns.
+# Find column names missing -mean() or -std(), and remove them.
+# Note: columns with meanfreq() are NOT kept.
+
 removeThese <- grep("(-mean\\(\\)|-std\\(\\))", names(mergedData), invert=T)
 mergedData[,c(removeThese) := NULL]
 
@@ -56,7 +57,6 @@ subjects <- rbind(fread(subTrainPath), fread(subTestPath))
 
 mergedData[, activity := activities[,V2]]
 mergedData[, subject.id := subjects]
-
 
 # Assumed to mean only use measurements with -mean() and -std() in their name
 # in the tidy data set
