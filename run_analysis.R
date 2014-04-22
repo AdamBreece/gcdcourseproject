486b54c475308203dc33c34465228379896d30fd
AdamB <abreece@gmail.com>
2014-04-22 14:27:15 -0700

    Activity factors and subject.id added to mergedData.

--- run_analysis.R
+++ run_analysis.R
@@ -2,25 +2,52 @@
 library(data.table)
 library(plyr)
 
-#### PART 1: Create a merged, cleaned data set ####
+# File paths
+# Assumes working directory has "UCI HAR Dataset" directory which
+# contains the dataset.
 
-## Step 0: Read column names
-featureNames <- read.table("features.txt")[,2]
+dataDir <- "UCI HAR Dataset"
+featuresPath <- paste(dataDir, "features.txt", sep = "/")
+labelPath <- paste(dataDir, "activity_labels.txt", sep = "/")
+
+xTrainPath <- paste(dataDir, "train/X_train.txt", sep = "/")
+yTrainPath <- paste(dataDir, "train/y_train.txt", sep = "/")
+subTrainPath <- paste(dataDir, "train/subject_train.txt", sep = "/")
+
+xTestPath <- paste(dataDir, "test/X_test.txt", sep = "/")
+yTestPath <- paste(dataDir, "test/y_test.txt", sep = "/")
+subTestPath <- paste(dataDir, "test/subject_test.txt", sep = "/")
+
+#### PART 1: Create a merged, cleaned data set ####
+print("Reading dataset, this may take a bit...")
+
+## Step 0: Read and prep metadata
+
+featureNames <- as.character(read.table(featuresPath)[,2])
+actLabels <- fread(labelPath)
+setkey(actLabels, V1)
+actLabels[,V2 := factor(actLabels[,V2])]
 
 ## Step 1: Merge the training and the test sets to create one data set.
 
 # The blank line at the start of X_test and X_train breaks fread (known bug) 
 # so we read it with read.table first, then make it a data.table.
-testFrame <- read.table("test/X_test.txt", col.names=featureNames)
-trainFrame <- read.table("train/X_train.txt", col.names=featureNames)
 
-mergedData <- rbindlist(list(trainFrame, testFrame))
+trainFrame <- read.table(xTrainPath, col.names=featureNames)
+testFrame <- read.table(xTestPath, col.names=featureNames)
 
-activities <- rbind(fread("test/y_test.txt"), fread("train/y_train.txt"))
-setnames(activities,"Activity")
+mergedData <- rbindlist(list(trainFrame, testFrame))	# returns data.table
+setnames(mergedData, featureNames)
 
-## Step 3: Appropriately label the activities. 
+# use data.table to join activity labels to activities
+activities <- rbind(fread(yTrainPath), fread(yTestPath))
+setkey(activities,V1)
+activities <- activities[actLabels]
 
+subjects <- rbind(fread(subTrainPath), fread(subTestPath))
+
+mergedData[, activity := activities[,V2]]
+mergedData[, subject.id := subjects]
 
 ## Step 4 (was 2): Extract only the measurements on the mean and 
 ## 			 standard deviation for each measurement.
